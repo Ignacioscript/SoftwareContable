aef53a81d475ec5a429c40704bdb0a702c181287
Ignacio Navarro <ignacio.script@gmail.com>
2022-09-28 09:56:52 -0500

    Creación del proyecto con spring boot, maven JDK 11

---
+++ SoftwareContable/src/main/java/edu/UdA/SoftwareContable/SoftwareContableApplication.java
@@ -0,0 +1,13 @@
+package edu.UdA.SoftwareContable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareContableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareContableApplication.class, args);
+	}
+
+}
---
+++ SoftwareContable/src/test/java/edu/UdA/SoftwareContable/SoftwareContableApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.UdA.SoftwareContable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareContableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
